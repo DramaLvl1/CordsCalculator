6c0203b9198f4cf0567462b2e3c575725473e235
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2023-01-28 00:12:20 +0100

    Update CordsC.py
    little bug fix

--- CordsC.py
+++ CordsC.py
@@ -1,22 +1,23 @@
-import time
+# import time
 
 print("Coords format: x:y (example: 235:266)")
-time.sleep(2)
+# time.sleep(2)
 
 firstcord = input("First coordinate")
 
 secondcord = input("Second coordinate")
-
 
 def sepcords(cord1, cord2):
   ncord1 = cord1.replace(":", " ")
   ncord2 = cord2.replace(":", " ")
   
   ncord1 = ncord1.split(" ")
-  ncord2 = ncord.split(" ")
+  ncord2 = ncord2.split(" ")
   
-  x = ncord1[0] - ncord2[0]
-  y = ncord2[1] - ncord2[1]
+  x = int(ncord1[0]) - int(ncord2[0])
+  y = int(ncord2[1]) - int(ncord2[1])
   result = x + y
   
   print("Distance:" + str(result**0.5))
+  
+sepcords(firstcord, secondcord)
